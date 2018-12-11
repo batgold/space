16932729ff2037c5e2fa3d94abb4d7aed8c32549
ben <batgold@gmail.com>
2018-12-11 14:52:42 -0800

    first graph

--- graph.py
+++ graph.py
@@ -8,13 +8,17 @@ def graph(name, lat, lon, alt):
     ax = fig.add_subplot(111, projection='3d')
 
     for n, item in enumerate(name):
-        if 'R/B' in item:
-            ax.scatter(lon[n], alt[n], zs=lat[n], s=10, c='r', marker='^')
-        else:
-            ax.scatter(lon[n], alt[n], zs=lat[n], s=10, c='C0', alpha=0.5)
+        if 'R/B' in item and nmp.abs(lon[n]) < 30:
+            ax.scatter(lon[n], lat[n], zs=alt[n], s=10, c='r', marker='^')
+        elif nmp.abs(lon[n]) < 30:
+            ax.scatter(lon[n], lat[n], zs=alt[n], s=10, c='C0', alpha=0.5)
 
     ax.set_xlim([-30, 30])
-    ax.set_ylim([30000, 40000])
+    ax.set_zlim([32000, 40000])
+
+    ax.set_xlabel('Longitude, deg')
+    ax.set_ylabel('Latitude, deg')
+    ax.set_zlabel('Altitude, km')
 
     ax.xaxis.pane.fill = False
     ax.yaxis.pane.fill = False
